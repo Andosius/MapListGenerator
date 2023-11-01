e5f1f4674a1cbf733f60d735bdecd7ff59a71e6f
Denis Ahmatovic <37641046+Andosius@users.noreply.github.com>
2023-11-01 18:09:23 +0100

    Remove old comments

--- src/Utility.hpp
+++ src/Utility.hpp
@@ -12,27 +12,6 @@
 
 namespace Utility
 {
-	/*void SetSevenZipPath(std::string& sevenZipPath);
-
-	std::string GetCommandResult(const std::string command);
-
-	bool IsValidMetaFile(const std::string& content);
-
-	std::string GetFilteredOutputLine(const std::string& line);
-
-	std::string ExtractMapName(const std::string& xmlContent);
-
-	int GetAllArchives(const std::string& base_path, moodycamel::ConcurrentQueue<std::string>& queue);
-
-	bool IsZipArchive(const std::string& filepath);
-
-	std::vector<std::string> GetMetaFiles(const std::string& archive);
-
-	std::string GetMetaFileContent(const std::string& archive, const std::string& file);
-
-	int CalculateThreads();*/
-
-	// REWRITE
 	int CalculateThreads();
 	bool IsAnyArchive(const std::string& filepath);
 	bool IsZipArchive(const std::string& filepath);
